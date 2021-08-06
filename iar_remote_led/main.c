1989e89c45247ccdbe71dd23a379d281852df849
DenisBSUIR <48877018+DenisBSUIR@users.noreply.github.com>
2021-08-06 15:36:41 +0300

    Add files via upload

--- iar_remote_led/main.c
+++ iar_remote_led/main.c
@@ -12,12 +12,16 @@
 
 #include "inc/tm4c123gh6pm.h"
 
+/*! \brief Структура, отражающая содержание получаемого байта 
+ */
 struct receive_packet{
 	uint8_t red: 1;
 	uint8_t green: 1;
 	uint8_t blue: 1;
 };
 
+/*! \brief Структура, отражающая содержание байта, отправляемого на сервер при нажатии какой-либо кнопки 
+ */
 struct transmit_packet {
 	uint8_t red: 1;
 	uint8_t green: 1;
@@ -30,14 +34,18 @@ void board_init()
 {
 }
 
+//флаг получения пакета
 bool received = false;
 
+/*! \brief Обработчик прерывания при появлении пакета на приеме
+ */
 void UART0_Handler(void)
 {
-	uint32_t status;
-	status = UARTIntStatus(UART0_BASE, true);
+	//читается и сбрасывается статус прерывания
+	uint32_t status = UARTIntStatus(UART0_BASE, true);
 	UARTIntClear(UART0_BASE, status); 
 
+	//получен пакет
 	received = true;
 }
 
@@ -45,51 +53,65 @@ int main()
 {
 	HwSetup();
 
-	bool btn1_was_pressed = false;
-	bool btn2_was_pressed = false;
+	//состояние светодиодов
+	struct receive_packet led_state = {0, 0, 0};
 	
 	while(true) {
 	
 		if(received) {
 			
+			//обработка полученного пакета
 			while(UARTCharsAvail(UART0_BASE))
 			{
+				//функция получения символа возвражает его приведя к int32_t
 				int32_t casted_character = UARTCharGetNonBlocking(UART0_BASE);
 				uint8_t character = ((uint8_t*)&casted_character)[0];
+				struct receive_packet recv_packet = *((struct receive_packet*)&character);
 		
-				struct receive_packet packet = *((struct receive_packet*)&character);
-		
-				if(packet.red) led_toggle(LED_RED);
-				if(packet.green) led_toggle(LED_GREEN);
-				if(packet.blue) led_toggle(LED_BLUE);
+				//изменение и сохранение состояния диодов
+				if(recv_packet.red) 
+				{
+					led_toggle(LED_RED);
+					led_state.red = led_state.red ? 0 : 1;
+				}
+				if(recv_packet.green)
+				{
+					led_toggle(LED_GREEN);
+					led_state.green = led_state.green ? 0 : 1;
+				}
+				if(recv_packet.blue)
+				{
+					led_toggle(LED_BLUE);
+					led_state.blue = led_state.blue ? 0 : 1;
+				}
 			}
 			
+			//сброс флага
 			received = false;
 		}
 		
-		bool current_btn2 = isButtonPressed(BTN_1);
-		bool current_btn1 = isButtonPressed(BTN_2);
-		
-		
-		if((!btn1_was_pressed && current_btn1) || (!btn2_was_pressed && current_btn2)) {
-		
+		//обработка нажатий кнопок
+		uint8_t button_pressed = bspBtnPolling();
+		if(button_pressed)
+		{
+			//если какая-либо кнопка была нажата необходимо подготовить пакет
 			struct transmit_packet packet = {0, 0, 0, 0, 0};
-
-			packet.red = (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1)& GPIO_PIN_1) >> 1;
-			packet.blue = (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2)& GPIO_PIN_2) >> 2;
-			packet.green = (GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_3) & GPIO_PIN_3) >> 3;
-
-//			if(current_btn1) packet.btn1 = 1;
-//			if(current_btn2) packet.btn2 = 1;
-			packet.btn1 = current_btn1;
-			packet.btn2 = current_btn2;
+			packet.red = led_state.red;
+			packet.green = led_state.green;
+			packet.blue = led_state.blue;
 			
-			UARTCharPutNonBlocking(UART0_BASE, *((uint8_t*)&packet));
+			switch(button_pressed)
+			{
+			case 1:
+				packet.btn2 = 1;
+				break;
+			case 2:
+				packet.btn1 = 1;
+				break;
+			}
 			
+			UARTCharPut(UART0_BASE, *((uint8_t*)&packet));
 		}
-		
-		btn1_was_pressed = current_btn1;
-		btn2_was_pressed = current_btn2;
 	}
 
 	return 0;
