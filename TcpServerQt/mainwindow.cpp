e729a92dbad08544a20b5a0e4bf319c47e506c0b
Anastasiya Rudauskaya <48052064+AsyaStrelk@users.noreply.github.com>
2021-08-06 09:55:49 +0200

    Update mainwindow.cpp
    1)упрощено тело цикла определения присутствующих на сервере портов
    2)исправлены проблемы с неоднократным выделением памяти под объекты сервера и порта
    3)добавлены некоторые проверки
    4)исправлена функция connectCOM();

--- TcpServerQt/mainwindow.cpp
+++ TcpServerQt/mainwindow.cpp
@@ -7,31 +7,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QWidget::setWindowTitle("TcpServer");
+    this->setWindowTitle("TcpServer");
 
-       textField = new QTextEdit(" ",this);
-       textField->setGeometry(QRect(QPoint(20,20),QSize(420,360)));
-       textField->setReadOnly(true);
+    textField = new QTextEdit(" ",this);
+    textField->setGeometry(QRect(QPoint(20,20),QSize(420,360)));
+    textField->setReadOnly(true);
 
-       startButton = new QPushButton("Start", this);
-       startButton->setGeometry(QRect(QPoint(20,420),QSize(200,40)));
-       connect(startButton, SIGNAL(released()), this, SLOT(startButtonClicked()));
+    startButton = new QPushButton("Start", this);
+    startButton->setGeometry(QRect(QPoint(20,420),QSize(200,40)));
+    connect(startButton, SIGNAL(released()), this, SLOT(startButtonClicked()));
 
-       stopButton = new QPushButton("Stop", this);
-       stopButton->setGeometry(QRect(QPoint(240,420),QSize(200,40)));
-       connect(stopButton, SIGNAL(released()), this, SLOT(stopButtonClicked()));
-       stopButton->setEnabled(false);
+    stopButton = new QPushButton("Stop", this);
+    stopButton->setGeometry(QRect(QPoint(240,420),QSize(200,40)));
+    connect(stopButton, SIGNAL(released()), this, SLOT(stopButtonClicked()));
+    stopButton->setEnabled(false);
 
-       serialPortBox=new QComboBox(this);
-       serialPortBox->setGeometry(QRect(QPoint(20,390),QSize(420,20)));
+    serialPortBox=new QComboBox(this);
+    serialPortBox->setGeometry(QRect(QPoint(20,390),QSize(420,20)));
 
-       //get serialports
-       const auto infos = QSerialPortInfo::availablePorts();
-       for (const QSerialPortInfo &info : infos) {
-           QStringList list;
-           list << info.portName();
-           serialPortBox->addItem(list.first());
-       }
+    tcpServer = new QTcpServer(this);
+    connect(tcpServer, SIGNAL(newConnection()), this, SLOT(addNewUser()));
+    serialPort = new QSerialPort(this);
+    connect(serialPort, SIGNAL(readyRead()), this, SLOT(slotReadComPort()));
+
+    //get serialports
+
+    const auto infos = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &info : infos) {
+        serialPortBox->addItem(info.portName());
+    }
 }
 
 void MainWindow::startButtonClicked()
@@ -56,6 +60,16 @@ void MainWindow::stopButtonClicked()
     closeSerialPort();
 }
 
+void MainWindow::startServer()
+{
+    if (!tcpServer->listen(QHostAddress::Any, 9090) && server_status==0) {
+        textField->append(tcpServer->errorString());
+        } else {
+        server_status=1;
+        textField->append(tcpServer->isListening()+" TCPSocket listen on port");
+    }
+}
+
 void MainWindow::stopServer()
 {
     if(server_status==1){
@@ -67,18 +81,6 @@ void MainWindow::stopServer()
         tcpServer->close();
         server_status=0;
         textField->append("server was stopped...");
-    }
-}
-
-void MainWindow::startServer()
-{
-    tcpServer = new QTcpServer(this);
-    connect(tcpServer, SIGNAL(newConnection()), this, SLOT(addNewUser()));
-    if (!tcpServer->listen(QHostAddress::Any, 9090) && server_status==0) {
-        textField->append(tcpServer->errorString());
-        } else {
-        server_status=1;
-        textField->append(tcpServer->isListening()+" TCPSocket listen on port");
     }
 }
 
@@ -118,7 +120,10 @@ void MainWindow::slotReadClient()
 void MainWindow::slotReadComPort()
 {
     const QByteArray data = serialPort->readAll();
-
+    if (data.size()!=1){
+    textField->append("data reading from port error");
+    stopButtonClicked();
+    }
     textField->append(tr("data from COM port:%1%2%3%4%5%6%7%8").arg((data[0]&128)!=0)
                       .arg((data[0]&64)!=0).arg((data[0]&32)!=0).arg((data[0]&16)!=0)
                       .arg((data[0]&8)!=0).arg((data[0]&4)!=0).arg((data[0]&2)!=0).arg((data[0]&1)!=0));
@@ -131,13 +136,10 @@ void MainWindow::slotReadComPort()
 
 void MainWindow::connectCOM()
 {
-    serialPort = new QSerialPort(this);
-    connect(serialPort, SIGNAL(readyRead()), this, SLOT(slotReadComPort()));
-
     serialPort->setPortName(serialPortBox->currentText());
-    serialPort->setBaudRate(CBR_115200);
-    serialPort->setReadBufferSize(8);
     if (serialPort->open(QIODevice::ReadWrite)) {
+        serialPort->setBaudRate(CBR_115200);
+        serialPort->setReadBufferSize(8);
         textField->append(tr("Connected to %1 : %2, %3, %4, %5, %6").arg(serialPort->portName())
                           .arg(serialPort->baudRate()).arg(serialPort->dataBits())
                           .arg(serialPort->parity()).arg(serialPort->stopBits()).arg(serialPort->flowControl()));
@@ -150,15 +152,18 @@ void MainWindow::closeSerialPort()
 {
     if (serialPort->isOpen())
         serialPort->close();
-    textField->append("COM port was disconnected");
+    textField->append("COM port was disconnected");   
 }
 
 MainWindow::~MainWindow()
 {
     closeSerialPort();
     stopServer();
+    delete(serialPort);
+    delete(tcpServer);
     delete ui;
 }
+
 
 
 
